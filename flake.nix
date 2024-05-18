1d5e6d0ccdb28e98925c168cccdabd92847e9a04
Evan Relf <evan@evanrelf.com>
2024-05-17 21:17:02 -0700

    Add wrapper for `home-manager`

--- flake.nix
+++ flake.nix
@@ -16,16 +16,20 @@
         pkgs = import nixpkgs { inherit system; };
       in
       {
+        # Packaging up your Home Manager configuration.
+        packages.homeConfigurations.default =
+          inputs.home-manager.lib.homeManagerConfiguration {
+            inherit pkgs;
+            modules = [ ./home.nix ];
+            extraSpecialArgs = { inherit inputs; };
+          };
+
+        # A wrapper around the `home-manager` program to point it at this flake.
+        # Run it with `nix run . -- <options>`, e.g. `nix run . -- switch`.
         packages.default =
-          let
-            homeConfiguration =
-              inputs.home-manager.lib.homeManagerConfiguration {
-                inherit pkgs;
-                modules = [ ./home.nix ];
-                extraSpecialArgs = { inherit inputs; };
-              };
-          in
-          homeConfiguration.activationPackage;
+          pkgs.writeShellScriptBin "home-manager" ''
+            exec ${pkgs.home-manager}/bin/home-manager --flake .#default $@
+          '';
       }
     );
 }
