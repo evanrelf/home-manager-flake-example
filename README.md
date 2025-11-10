013456fbff021b3489a0170f92676f789c9e62c1
Evan Relf <evan@evanrelf.com>
2025-11-10 00:26:19 -0800

    Add more links and recommendations

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Home Manager flake example
 
-Example of using Home Manager with Nix flakes. Copy `flake.nix` and `home.nix`,
-address the todos in `home.nix`, and then you're all set up!
+Example of using [Home Manager] with [Nix flakes]. Copy `flake.nix` and
+`home.nix`, address the todos in `home.nix`, and then you're all set up!
+
+[Home Manager]: https://github.com/nix-community/home-manager
+[Nix flakes]: https://wiki.nixos.org/wiki/Flakes
--- home.nix
+++ home.nix
@@ -1,10 +1,19 @@
 { config, inputs, lib, pkgs, ... }:
 
+# Find documentation for Home Manager options here:
+# https://nix-community.github.io/home-manager/options.xhtml
+#
+# I recommend looking at `home.packages` for package installation, and
+# `home.file` and `xdg.configFile` for configuration file linking.
+
 {
   # TODO: Choose the latest version from this list when you first set up Home
   # Manager, and then leave it alone thereafter.
   # https://nix-community.github.io/home-manager/options.xhtml#opt-home.stateVersion
   home.stateVersion = "25.11";
+
+  # TODO: Optionally uncomment this to silence Home Manager's news message.
+  # news.display = "silent";
 
   # TODO: Change this to your username.
   home.username = "evanrelf";
